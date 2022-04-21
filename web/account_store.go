b2929f09bef7be0577cecdc93a67e5396849cc9d
Ception <ception@live.ca>
2022-04-21 19:04:46 -0400

    - Finished UpdateAccount function in account_store

--- web/account_store.go
+++ web/account_store.go
@@ -19,8 +19,7 @@ type AccountStore struct {
 
 func (s *AccountStore) Account(id int) (goarcane.Account, error) {
 	var t goarcane.Account
-	err := s.Get(&t, `SELECT * FROM accounts WHERE id = $1`, id)
-	if err != nil {
+	if err := s.Get(&t, `SELECT * FROM accounts WHERE id = $1`, id); err != nil {
 		return goarcane.Account{}, fmt.Errorf("[ERROR] Getting Account: %w", err)
 	}
 	return t, nil
@@ -28,27 +27,32 @@ func (s *AccountStore) Account(id int) (goarcane.Account, error) {
 
 func (s *AccountStore) Accounts() ([]goarcane.Account, error) {
 	var tt []goarcane.Account
-	err := s.Select(&tt, `SELECT * FROM accounts`)
-	if err != nil {
+	if err := s.Select(&tt, `SELECT * FROM accounts`); err != nil {
 		return []goarcane.Account{}, fmt.Errorf("[ERROR] Getting Accounts: %w", err)
 	}
 	return tt, nil
 }
 
 func (s *AccountStore) CreateAccount(a *goarcane.Account) error {
-	err := s.Get(a, `INSERT INTO accounts VALUES ($1, $2, $3, $4) RETURNING *`,
+	if err := s.Get(a, `INSERT INTO accounts VALUES ($1, $2, $3, $4) RETURNING *`,
 		a.ID,
 		a.Username,
 		a.Password,
-		a.Email)
-	if err != nil {
+		a.Email); err != nil {
 		return fmt.Errorf("[ERROR] Creating Account: %w", err)
 	}
 	return nil
 }
 
 func (s *AccountStore) UpdateAccount(a *goarcane.Account) error {
-	panic("not implemented") // TODO: Implement
+	if err := s.Get(a, `UPDATE accounts SET name = $1, password = $2, email = $3 WHERE id = $4 RETURNING *`,
+		a.Username,
+		a.Password,
+		a.Email,
+		a.ID); err != nil {
+		return fmt.Errorf("[ERROR] Updating Account: %w", err)
+	}
+	return nil
 }
 
 func (s *AccountStore) DeleteAccount(id int) error {
